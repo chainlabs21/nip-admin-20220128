92dd5f5f64487f458f517231937bfb80e92f6419
leejh16 <cheny5dyh@gmail.com>
2022-03-03 00:22:42 +0900

    fetch items-20220303

--- src/configs/api.js
+++ src/configs/api.js
@@ -3,10 +3,33 @@ const URL='https://nftinfinity.world:34825' // nips1.net:34815'
 
 const API={
 	API_TXS_STAKE : URL + '/queries/rows/transactions/typestr/STAKE' // /:offset/:limit/:orderkey/:orderval
-		, API_USERS : URL + '/queries/rows/users/active/1' // /:offset/:limit/:orderkey/:orderval
-
+	, API_USERS : URL + '/queries/rows/users/active/1' // /:offset/:limit/:orderkey/:orderval
+	, API_COMMONITEMS  : URL + '/queries/rows'
 }
 
 export {
 	API
 }
+
+// const URL='http://nip s1.net:348 15'
+// const URL='http://3.35. 117.87:34 815'
+/**  const API={
+		API_MAX	: URL + `/queries/max` // /:tablename/:fieldname
+	, API_TXS	: URL + '/transactions' // /:txhash
+	, API_TICKERS : URL + '/tickers'
+	, API_USERINFO : URL + '/users/info'
+	, API_TOGGLE_FAVORITE : URL + '/favorites/toggle'
+	, API_LOGIN : URL + '/users/login'
+
+	, API_EMAIL_REQUEST : URL + '/signup/email/request'
+	, API_SIGNUP : URL + '/signup/signup'
+	, API_QUERY_REFERER : URL + '/queries/singlerow' // /:tablename/:fieldname/:fieldval
+	, API_QUERY_USERADDRESS : URL + '/queries/singlerow' 
+	, API_PREMIUMITEMS : URL + '/queries/rows'
+	, API_COMMONITEMS  : URL + '/queries/rows'
+	, API_ITEMDETAIL : URL + '/items/item' // /:itemid
+}
+export { 
+	API
+}
+*/
--- src/page/nft-managing/NftManaging-2rows.tsx
+++ src/page/nft-managing/NftManaging-2rows.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import Papers from '../../components/paper/Papers'
@@ -9,11 +9,6 @@ import Searches from '../../components/input/search/Searches'
 import ContainedButton from '../../components/input/button/ContainedButton'
 import eg_image from '../../assets/images/ex-image.png'
 import { Pagination } from '@mui/material'
-import Toggle from 'react-toggle'
-import 'react-toggle/style.css'
-import { API } from '../../configs/api';
-import { LOGGER } from '../../utils/common';
-import axios from 'axios'
 
 const fields_01 = [
   {
@@ -118,19 +113,6 @@ const fields_02 = [
 ]
 
 const NftManaging = () => {
-	let [ list , setlist] = useState( [] )
-	const fetchdata=()=>{
-		axios.get(API.API_COMMONITEMS + `/items/group_/kong/0/32/id/DESC` ).then(resp=>{	
-			LOGGER('' , resp.data )
-			let { status , list }=resp.data
-			if ( status =='OK'){
-				setlist ( list.slice(0,10) )
-			}
-		})
-	}
-	useEffect(()=>{
-		fetchdata()
-	} , [] )
   return (
     <>
       <div
@@ -273,7 +255,7 @@ const NftManaging = () => {
                     <td className="nft-td">입찰 참여 시작일</td>
                     <td className="nft-td">생성일</td>
                   </tr>
-{/**                   <tr>
+                  <tr>
                     <td className="nft-td">스테이킹 지급 일수</td>
                     <td className="nft-td">스테이킹 지급 토큰</td>
                     <td className="nft-td">상태</td>
@@ -281,52 +263,18 @@ const NftManaging = () => {
                     <td className="nft-td">설정된 수익률</td>
                     <td className="nft-td">입찰 참여 종료일</td>
                     <td className="nft-td">다음 매칭</td>
-                  </tr>*/}
+                  </tr>
                 </thead>
 
                 <tbody>
-									{list.map((elem:any , idx : number)=>(
-										<tr>
-										<td className="nft-td" rowSpan={1}>
-											{elem.id}
-										</td>
-										<td className="nft-td" rowSpan={1}>
-											<img src={elem.url } style={{height:'50px'}} alt="eg_image" />
-										</td>
-										<td className="nft-td" rowSpan={1}>
-											0xb6..2ef0
-										</td>
-										<td className="nft-td">NIP</td>
-										<td className="nft-td">100</td>
-										<td className="nft-td">3</td>
-										<td className="nft-td">126</td>
-										<td className="nft-td">8%</td>
-										<td className="nft-td">
-											<input
-												type="date"                        id="start"
-												name="trip-start"                        value="2022-02-02"
-												min="2022-02-02"                        max="2022-03-03"
-												style={{                          width: '100%',
-													height: '40px',                          borderRadius: '12px',
-													border: '1px solid #D9D9D9',                          textAlign: 'center',
-												}}
-											/>
-										</td>
-										<td className="nft-td" rowSpan={1}>
-											<Toggle
-		defaultChecked={false}
-		disabled={false } icons={false} /><br/><span>On sale</span>
-										</td>
-									</tr>
-									))}
-{/**                   <tr>
-                    <td className="nft-td" rowSpan={1}>
+                  <tr>
+                    <td className="nft-td" rowSpan={2}>
                       1
                     </td>
-                    <td className="nft-td" rowSpan={1}>
-                      <img src={eg_image} style={{height:'50px'}} alt="eg_image" />
+                    <td className="nft-td" rowSpan={2}>
+                      <img src={eg_image} alt="eg_image" />
                     </td>
-                    <td className="nft-td" rowSpan={1}>
+                    <td className="nft-td" rowSpan={2}>
                       0xb6..2ef0
                     </td>
                     <td className="nft-td">NIP</td>
@@ -336,22 +284,24 @@ const NftManaging = () => {
                     <td className="nft-td">8%</td>
                     <td className="nft-td">
                       <input
-                        type="date"                        id="start"
-                        name="trip-start"                        value="2022-02-02"
-                        min="2022-02-02"                        max="2022-03-03"
-                        style={{                          width: '100%',
-                          height: '40px',                          borderRadius: '12px',
-                          border: '1px solid #D9D9D9',                          textAlign: 'center',
+                        type="date"
+                        id="start"
+                        name="trip-start"
+                        value="2022-02-02"
+                        min="2022-02-02"
+                        max="2022-03-03"
+                        style={{
+                          width: '100%',
+                          height: '40px',
+                          borderRadius: '12px',
+                          border: '1px solid #D9D9D9',
+                          textAlign: 'center',
                         }}
                       />
                     </td>
-                    <td className="nft-td" rowSpan={2}>											
-											<Toggle
-    defaultChecked={false}
-    disabled={false } icons={false} /><br/><span>On sale</span>
-										</td>
-                  </tr>*/}
-{/**                   <tr>
+                    <td className="nft-td">다음 매칭</td>
+                  </tr>
+                  <tr>
                     <td className="nft-td">100</td>
                     <td className="nft-td">100</td>
                     <td className="nft-td">Stay</td>
@@ -375,7 +325,7 @@ const NftManaging = () => {
                       />
                     </td>
                     <td className="nft-td">2022-02-02</td>
-                  </tr>*/}
+                  </tr>
                 </tbody>
               </table>
             </div>
