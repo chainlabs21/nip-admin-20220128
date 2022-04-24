eb5ae10dff05969dc48fbbc12db0ba6f3fb9f5d6
Saddam-tech <salokhiddinov0727@gmail.com>
2022-04-24 11:43:00 +0900

    fixed the margin for the wrapper of the Routes

--- src/page/dashboard/Dashboard.tsx
+++ src/page/dashboard/Dashboard.tsx
@@ -214,10 +214,10 @@ function DashboardContent() {
           <Toolbar />
 
           <Container
-            sx={{ mt: 4, mb: 4, ml: 25, width: '100%', maxWidth: '1526px' }}
+            sx={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", width: '100%', height: "100vh" }}
           >
             <Routers />
-            <Copyright sx={{ pt: 4 }} />
+            <Copyright sx={{ pt: 4, paddingBottom: "30px" }} />
           </Container>
         </Box>
       </Box>
